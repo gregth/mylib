19216ae7667f96cc98883cd90d55d0d25ffb0aea
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2015-07-18 00:34:59 +0300

    Editer register controller

--- register.php
+++ register.php
@@ -1,45 +1,41 @@
 <?php
-  require 'debug.php';
-  require 'models/validation_functions.php';
-  require 'views/header.php';
-  if ( !empty( $_POST ) ) 
-  {
-    //inludes
-    include 'connect.php';
-    include 'db_functions.php';
-    include 'validation_functions.php';
-    $res = get_data_errors($_POST) //get data errors returns a table with all errors
-    if (empty($res)) // if there are no errors continue with the reg
+    require 'debug.php';
+    require 'models/validation_functions.php';
+    require 'views/header.php';
+    if ( !empty( $_POST ) )
     {
+        include 'connect.php';
+        include 'db_functions.php';
+        include 'validation_functions.php';
+
+        //get data errors returns a table with all errors
+        $errors = get_data_errors($_POST)
+        // if there are no errors continue with the reg
+        if (empty($errors))
+        {
             //insert user info to database
-        $success = register_user($_POST)
-        if ($success)
-        {
-            $_SESSION[ 'username' ] = $username;
-            $_SESSION[ 'userid' ] = mysql_insert_id();
-            header("location : $absPath.index.php ")
-            die();
-        }
-        else 
-        {
+            $success = register_user($_POST)
+            if ($success)
+            {
+                $_SESSION[ 'username' ] = $username;
+                $_SESSION[ 'userid' ] = mysql_insert_id();
+                header("location : index.php ")
+                die();
+            }
+            else
+            {
                 $errors = 'internal database error try again' // if sql fails we get this error
+            }
         }
-    }
-    else
-    {
-            foreach($res as $value)
+        else
         {
-            $errors.=$value; // $errors has  all the found errors
+            //Include view that parses errors and form view
+            require 'views/form_errors.php';
+            require 'views/registration.php';
         }
     }
-  }
-  else {
-      require 'form_errors.php';
-      require 'models/registration.php';
+    else {
+            require 'views/register_form.php';
     }
-  }
-  else {
-    require 'views/register_form.php';
-  }
-  require 'views/footer.php';
+    require 'views/footer.php';
 ?>
