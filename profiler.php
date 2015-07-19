46c0457dd0112af7e58619a9c2c3f6a0c5f32fab
Alexandros Kalimeris <alexandros.kal@hotmail.com>
2015-07-19 16:55:02 +0300

    added profile view controller and a function to get user data

---
+++ profiler.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    require 'models/db_functions.php';
+    // if note logged in , redirect to login page
+    if ( !isset( $_SESSION[ 'userid' ] ) ) {
+        header( 'Location: login.php' );
+    }
+    // get user data from db
+    $data = getUserData($_SESSION[ 'userid' ] );
+    require 'views/header.php';
+    require 'views/profile.php';
+    require 'views/footer.php';
+?>
