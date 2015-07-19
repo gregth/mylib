5e5d4ffa337d11cee466f4d6d9353fb270f92e2d
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2015-07-19 22:09:33 +0300

    Various fixes

--- add_book.php
+++ add_book.php
@@ -1,4 +1,5 @@
 <?php
+    require 'models/connect.php';
     require 'models/book_add_functions.php';
     if ( !empty( $_POST ) ) {
         //In this case adds the book
@@ -6,6 +7,7 @@
         require 'views/header.php';
         if ( !$errors ) {
             //TODO add book to database
+            addBook( $_POST );
             echo 'Book to be added';
         }
         else {
@@ -15,6 +17,14 @@
         require 'views/footer.php';
     }
     else {
+
+        //Make sure thatuser request at max 4 fields for author and at maxt 4 fields fot genres
+        $authors = getAuthorsNum( $_GET );
+        $genres = getGenresNum( $_GET ) ;
+        if ( $authors != $_GET[ 'authors' ] || $genres != $_GET[ 'genres' ] ) {
+            header( 'Location: add_book.php?authors=' . $authors . '&genres=' . $genres );
+            exit();
+        }
         require 'views/header.php';
         require 'views/add_book_form.php';
         require 'views/footer.php';
