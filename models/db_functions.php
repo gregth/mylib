e51434773e982d8a02b4226c352b503ea66da1fa
Alexandros Kalimeris <alexandros.kal@hotmail.com>
2015-07-19 12:17:52 +0300

    transitioning to mysqli

--- models/db_functions.php
+++ models/db_functions.php
@@ -11,19 +11,11 @@
         $email = $data['email'];
         $hash = hash_fun($data['password']); //hash with salt at the end
         list($passhash,$salt) = explode("^",$hash);
-        $success = mysql_query(
-            "
-            INSERT INTO
-                users
-            SET
-                username = '$username',
-                password = '$passhash',
-                salt = '$salt',
-                first_name = '$first_name',
-                last_name = '$last_name',
-                email = '$email'
-            ;"
-        );
+        $stmt = mysqli_prepare($db,
+            "INSERT INTO users SET username = ? , password = ? , salt = ? , first_name = ? , last_name = ? , email = ? ");
+        mysqli_stmt_bind_param($stmt,"ssssss",$username,$passhash,$salt,$first_name,$last_name,$email);
+        mysqli_stmt_execute($stmt);
+        $success = mysqli_affeted_rows($db);
         return $success;
     };
 
@@ -42,39 +34,31 @@
         $email = $data['email'];
         $password = $data['password'];
         //getting the salt
-        $res = mysql_query(
-            "
-            SELECT
-               salt
-            FROM
-                users
-            WHERE
-                email = '$email'
-            LIMIT
-                1
-            ;"
-
-        );
-        if (mysql_num_rows($res)==1)
+        $stmt = mysqli_prepare($db,
+            "SELECT salt FROM users WHERE email = ? LIMIT 1");
+        mysqli_stmt_bind_param($stmt,"s",$email);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_store_result($stmt);
+        mysqli_stmt_bind_result($stmt,$salt);
+        if (mysqli_stmt_num_rows($stmt))
         {
-            $tmp = mysql_fetch_array($res);
-            $salt = $tmp['salt'];
+  //          $tmp = mysql_fetch_array($res);
+    //        $salt = $tmp['salt'];
+            mysqli_stmt_fetch($stmt); 
             $password = hash('sha256',"$password"."$salt");
             //adding salt to given pass hashing and checking if the resulting hash is the same as the original
-            $res = mysql_query(
-                "
-                SELECT
-                    uid
-                FROM
-                    users
-                WHERE
-                    email = $email AND password = $password
-                ;"
-                );
-            if(mysql_num_rows($res)==1)
+            mysqli_stmt_close($stmt);
+            $stmt = mysql_query($db,
+                "SELECT uid FROM users WHERE email = ? AND password = ?");
+            mysqli_stmt_bind_param($stmt,"ss",$email,$password);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+            mysqli_stmt_bind_result($stmt,$user);
+            if(mysqli_stmt_num_rows($stmt))
             {
-                $user = mysql_fetch_array($res);
-                return $user['uid'];
+                mysqli_fetch_($stmt);
+                mysqli_stmt_close($stmt);
+                return $user;
             }
             else
             {
