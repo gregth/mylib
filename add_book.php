debcf31d04b58b4a0b852de7cf1c447eb9cdf8aa
Alexandros Kalimeris <alexandros.kal@hotmail.com>
2015-07-20 13:49:21 +0300

    fixed conflict

--- add_book.php
+++ add_book.php
@@ -7,11 +7,6 @@
         require 'views/header.php';
         if ( !$errors ) {
             //TODO add book to database
-            /*var_dump($_FILES);
-            $path = '/var/www/html/uploads/';
-            $image = imageUploader($_FILES, $path);
-            echo "<img src=\"http://localhost/uploads/$image\">";*/ 
-            // uncomment to test
         }
         else {
             require 'views/form_errors.php';
