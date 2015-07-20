a2d5108144f0d954c299d1e9debdf1c4cd26638c
Alexandros Kalimeris <alexandros.kal@hotmail.com>
2015-07-20 13:28:06 +0300

    under construction

--- add_book.php
+++ add_book.php
@@ -1,12 +1,17 @@
 <?php
     require 'models/book_add_functions.php';
+    require 'models/image_upload.php';
     if ( !empty( $_POST ) ) {
         //In this case adds the book
         $errors = bookDataErrors( $_POST );
         require 'views/header.php';
         if ( !$errors ) {
             //TODO add book to database
-            echo 'Book to be added';
+            /*var_dump($_FILES);
+            $path = '/var/www/html/uploads/';
+            $image = imageUploader($_FILES, $path);
+            echo "<img src=\"http://localhost/uploads/$image\">";*/ 
+            // uncomment to test
         }
         else {
             require 'views/form_errors.php';
