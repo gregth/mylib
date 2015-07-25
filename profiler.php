987ed419d88152ec898bf7391a210e4855406c12
Alexandros Kalimeris <alexandros.kal@hotmail.com>
2015-07-25 15:40:50 +0300

    implemented comment view and and a form to add comments

--- profiler.php
+++ profiler.php
@@ -1,6 +1,8 @@
 <?php
     require 'models/db_functions.php';
+    require 'models/comment_functions.php';
     // if note logged in , redirect to login page
+    // better replace userid with profileid as a better fitting name
     if ( !isset( $_SESSION[ 'userid' ] ) ) {
         header( 'Location: login.php' );
         die();
@@ -13,5 +15,7 @@
     }
     require 'views/header.php';
     require 'views/profile.php';
+    $comments = getProfileComments( $_GET[ 'uid' ] );
+    require 'views/profile_comments.php';
     require 'views/footer.php';
 ?>
