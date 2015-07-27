6d12c554bb6b22d7fd4b8b9f4f96293f49134529
Alexandros Kalimeris <alexandros.kal@hotmail.com>
2015-07-27 18:58:26 +0300

    added encdata on forms so it uploads properly

--- views/book_copy_form.php
+++ views/book_copy_form.php
@@ -1,7 +1,7 @@
-<form action = "add_book_cp.php<?php if ( isset( $_GET[ 'bid' ] ) ) echo '?bid=' . $_GET[ 'bid' ] ?>" method = "POST" >
+<form action = "add_book_cp.php<?php if ( isset( $_GET[ 'bid' ] ) ) echo '?bid=' . $_GET[ 'bid' ] ?>" method = "POST" enctype="multipart/form-data">
     Description :
     <input type = "text" name = "description" placeholder = "Add a description ..."/>
     Image :
-    <input type = "file" name = "userImage" placeholder = "Upload your image"/>
+    <input type = "file" name = "userImage" id = "img"  placeholder = "Upload your image"/>
     <input type = "submit" value = "Add"/>
 </form>
