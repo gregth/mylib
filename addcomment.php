41dac2b0d17a2e24431ef9715a808912d2a378fb
Alexandros Kalimeris <alexandros.kal@hotmail.com>
2015-07-27 18:52:54 +0300

    now adds book and profile comments depending on get parameter

--- addcomment.php
+++ addcomment.php
@@ -1,19 +1,34 @@
 <?php
+    require "models/connect.php";
     require "models/comment_functions.php";
-    require "models/db_functions.php";
+    require "models/user_functions.php";
+    require "models/show_book_functions.php";
+    require "models/show_bookcp_functions.php";
     //if seesion isn't set redirects to login page
     if (!isset( $_SESSION ) ) {
         header("Location: login.php");
         die();
     }
-    //TODO select for book or profile comment
+    if ( isset( $_GET[ 'profileid' ] ) ) {
     // if a comment is submited and profile id is valid add comment
-    if(!empty($_POST[ 'comment' ] && getUserData($_GET[ 'profileid' ] ) ) ) {
-        addProfileComment($_POST[ 'comment' ], $_SESSION[ 'userid' ], $_GET[ 'profileid' ] );
-        //redirects back to profile page
-        header("Location: profiler.php"."?uid=".$_GET[ 'profileid' ] );
-        die();
+        if(!empty($_POST[ 'comment' ] && getUserData($_GET[ 'profileid' ] ) ) ) {
+            $success = addProfileComment($_POST[ 'comment' ], $_SESSION[ 'userid' ], $_GET[ 'profileid' ] );
+            //redirects back to profile page
+            header("Location: profiler.php"."?uid=".$_GET[ 'profileid' ] );
+            die();
+        }
     }
+    
+    if (isset ( $_GET[ 'bcid' ] ) ) {
+        if(!empty($_POST[ 'comment' ] && getBookcpDetails($_GET[ 'bcid' ] ) ) ) {
+            $success = addBookComment($_POST[ 'comment' ], $_SESSION[ 'userid' ], $_GET[ 'bcid' ] );
+            //redirects back to bcopy page
+            header("Location: bookcp.php"."?bcid=".$_GET[ 'bcid' ] );
+            die();
+        }
+    }
+
+
     header( "Location: index.php" );
     die();
 ?>
