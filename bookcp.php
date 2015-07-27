f0803178f543b6c2a35ba37d9aa50fec78d00a3b
Alexandros Kalimeris <alexandros.kal@hotmail.com>
2015-07-27 18:54:27 +0300

    controller for showing book copies and their comments

---
+++ bookcp.php
@@ -0,0 +1,28 @@
+<?php
+//shows the bookcp page of the book copy specified by the ?bcid
+    include 'models/show_book_functions.php';
+    include 'models/show_bookcp_functions.php';
+    include 'models/comment_functions.php';
+    include 'models/connect.php';
+    //if get bcid not set redirects to index
+    if ( isset( $_GET[ 'bcid' ] ) ) {
+        //if bcid does not exist redirects to 404 page
+        if ( $bcopy = getBookcpDetails( $_GET[ 'bcid' ] ) ) {
+            require 'views/header.php';
+            require 'views/bookcp.php';
+            $book = getBookDetails ( $bcopy[ 'bid' ] );
+            require 'views/book.php';
+            $comments = getBcopyComments( $_GET[ 'bcid' ] );
+            require 'views/bookcp_comments.php';
+            require 'views/footer.php';
+        }
+        else {
+            header( 'Location: 404.php' );
+        }
+    }
+    else {
+         header( 'Location: index.php' );
+    }
+    
+?>
+
