4d761801de20a3a74deb61008273b52e30c7ce0e
Alexandros Kalimeris <alexandros.kal@hotmail.com>
2015-07-27 18:55:26 +0300

    added addBookCp function

--- models/add_book_cp_functions.php
+++ models/add_book_cp_functions.php
@@ -9,4 +9,23 @@
             return true;
         }
     }
+
+
+    //insert bid description and user image url into the bcopies table , on failure return false
+    function addBookCp( $getdata, $postdata ) {
+        global $db;
+        require 'image_upload.php';
+        $description = $postdata[ 'description' ];
+        $bid = $getdata[ 'bid' ];
+        $path = imageUpload( 'data/user_images/', 'userImage' );
+        $stmt = mysqli_prepare( $db, 'INSERT INTO bcopies SET bid = ?, uid = ?, description = ?,  image = ?, sold = 0 ' );
+        mysqli_stmt_bind_param( $stmt,'iiss',  $bid ,$_SESSION[ 'userid' ], $description, $path );
+        mysqli_execute( $stmt );
+        $bcid = mysqli_insert_id( $db );
+        $success = mysqli_affected_rows( $db );
+        if ( $success ) {
+            return true;
+        }
+        return false;
+    }
 ?>
