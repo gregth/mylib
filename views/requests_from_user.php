8c14df7ce109400c810685f75026d7b8b4410295
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2015-08-06 00:32:30 +0300

    Added views to show user transactions and the result of them

---
+++ views/requests_from_user.php
@@ -0,0 +1,21 @@
+<div class="group" >
+    <div class="container" >
+        <h1>Τα βιβλία που σας ζητήσατε</h1>
+        <?php
+        if ( empty( $requests ) ) {
+        ?></li>Δεν έχετε ζητήσει κάποιο βιβλίο ακόμη</li><?php
+        }
+        foreach ( $requests as $request ) {
+            ?><li>Ζήτησε από το χρήστη <a href="profile.php?uid=<?php
+            echo $request[ 'owner' ][ 'uid' ];
+            ?>" ><?php
+            echo $request[ 'owner' ][ 'username' ];
+            ?></a> το βιβλίο <a href="bookcp.php?bcid=<?php
+            echo $request[ 'bcid' ];
+            ?>" ><?php echo $request[ 'title' ];
+            ?></a>.
+            </li><?php
+        }
+        ?></ul>
+    </div>
+</div>
---
+++ views/requests_to_user.php
@@ -0,0 +1,22 @@
+<div class="group" >
+    <div class="container" >
+        <h1>Τα βιβλία που σας ζήτησαν</h1>
+        <?php
+        if ( empty( $requests ) ) {
+        ?></li>Κανένας χρήστης δε σας έχει ζητήσει κάποιο βιβλίο ακόμη</li><?php
+        }
+        foreach ( $requests as $request ) {
+            ?><li>Ο χρήστης <a href="profile.php?uid=<?php
+            echo $request[ 'uid' ];
+            ?>" ><?php
+            echo $request[ 'username' ];
+            ?></a> ζήτησε από εσάς το βιβλίο <a href="bookcp.php?bcid=<?php
+            echo $request[ 'bcid' ];
+            ?>" ><?php
+            echo $request[ 'title' ];
+            ?></a>
+            </li><?php
+        }
+        ?></ul>
+    </div>
+</div>
