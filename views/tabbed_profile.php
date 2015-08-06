94d0cd7fe5dc2744312b2e896adf664bada5b08c
Grigoris Thanasoulas <gregthanasoulas@gmail.com>
2015-08-06 17:01:05 +0300

    Tabbed profile views

---
+++ views/tabbed_profile.php
@@ -0,0 +1,11 @@
+<div class="tab-content">
+  <div id="info" class="tab-pane fade "><?php
+    require 'views/profile_info.php';
+  ?></div>
+  <div id="comments" class="tab-pane fade"><?php
+    require 'views/comments.php';
+  ?></div>
+  <div id="bcopies" class="tab-pane fade in active"><?php
+    require 'views/bookcp_list.php';
+  ?></div>
+</div>
