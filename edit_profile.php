ffe9936adb898a86b93937e604720f84091b378c
Alexandros Kalimeris <alexandros.kal@hotmail.com>
2015-08-13 19:57:36 +0300

    add e mail chanfe still draft

--- edit_profile.php
+++ edit_profile.php
@@ -1,22 +1,38 @@
 <?php
     require 'models/connect.php';
-    require 'models/image_upload.php';
+    require 'models/validation_functions.php';
+    require 'models/user_functions.php';
+
     if ( isset( $_SESSION ) ) {
-        if ( !empty($_FILES[ 'newimg' ] ) ) {
-            $path = imageUpload ( 'data/profile_imgs/', 'newimg'  );
-            $success = false;
-            if ($path ) {
-                $success = imageUpdate ($path, $_SESSION[ 'userid' ] );
+        $emailError = '';
+        $imageError = '';
+        if ( !empty( $_POST['newemail'] ) ) {
+            $valid = validateEmail( $_POST[ 'newemail' ] );
+            if ( $valid ) {
+                $success = updateEmail( $_POST[ 'newemail' ], $_SESSION[ 'userid' ] );
+                if( !$success ) {
+                    $emailError = 'το E-mail';
+                }
             }
-            if ( $success ) {
-                echo "Ολοκληρώθηκε";
-                die();
+            else {
+                $emailError = 'το E-mail';
             }
         }
+        if ( !empty( $_FILES[ 'newimg' ] ) ) {
+            $path = imageUpload ( 'data/profile_imgs/', 'newimg'  );
+            $success = false;
+            if ( $path ) {
+                $success = imageUpdate ($path, $_SESSION[ 'userid' ] );
+            }
+            if ( !$success ) {
+                $imageError = 'την Εικόνα';
+            }
+        }
+        if ( $emailError||$imageError ) {
+            echo "Προσπαθήστε ξάνα υπήρξε πρόβλημα με : $emailError $imageError ";
+        }
+        else {
+            echo 'Ολοκληρώθηκε.';
+        }
     }
-    echo "Προσπαθήστε ξανά";
-    die();
-
-
-
 ?>
